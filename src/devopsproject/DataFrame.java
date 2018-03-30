21c249fc78dda2ba974f279c7d405cb085c4aaf4
stephanewINFO <stephaniebarona@gmail.com>
2018-03-30 23:05:32 +0200

    creation de dataframes

--- src/devopsproject/DataFrame.java
+++ src/devopsproject/DataFrame.java
@@ -3,7 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package devopsproject;
+package src.devopsproject;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -11,4 +15,20 @@ package devopsproject;
  */
 public class DataFrame {
     
-}
+    public String label;
+    public ArrayList column;
+
+    public DataFrame(String label, ArrayList column) {
+        this.label = label;
+        this.column = column;
+    }
+    
+    public DataFrame(String nameFile){
+        File file = new File(nameFile);
+        
+    }
+
+        
+    }
+    
+
