5bf6d3beaafa303239d1b40e4190223cd1fc8b2d
riad_tulus <dr_benounnas@esi.dz>
2018-04-04 10:14:07 +0200

    first constructor

--- src/devopsproject/DataFrame.java
+++ src/devopsproject/DataFrame.java
@@ -4,11 +4,20 @@
  * and open the template in the editor.
  */
 package devopsproject;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
  * @author bleuzeb
  */
 public class DataFrame {
-    
+    private HashMap<String, ArrayList<E>> data;
+
+    public DataFrame(String[] labels, ArrayList<E>[]){
+        for (Iterator<String> i = labels.iterator(); i.hasNext();) {
+            String label = i.next();
+            this.data.put(label,null)
+        }
+    }
 }
