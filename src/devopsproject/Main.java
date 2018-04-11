c15907d36d9640085a0cd3f139c138077e08af88
DMyTryC <dmytryc@gmail.com>
2018-04-11 10:38:32 +0200

    Ajout des tests dans maven

--- src/devopsproject/Main.java
+++ src/devopsproject/Main.java
@@ -21,18 +21,6 @@ public class Main {
     // d.show();
     //d.head(3);
     d.tail(2);*/
-    HashMap<String,Integer> hash = new HashMap<>();
-    hash.put("b", 1);
-    hash.put("a", 2);
-
-
-    for (Map.Entry<String, Integer> entrySet : hash.entrySet()) {
-	String key = entrySet.getKey();
-	Integer value = entrySet.getValue();
-
-	System.out.println(key + " "+ value);
-
-    }
     //d.head("Age", 2);
      //d.last("Age", 2);
      //ArrayList toprint = d.data.get("Age");
