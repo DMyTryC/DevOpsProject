5aad0cc03593d7a38236cea08447c40a97273357
DMyTryC <dumitru.corini@gmail.com>
2018-04-14 17:07:19 +0200

    Cleaning up test files (#28)
    * Resolving conflicts
    
    * Removing cached files after gitignore change
    
    * Commiting the new gitignore
    
    * Adding the explanation of mvn site
    
    * Removing files that weren't supposed to be commited
    
    * Adding code coverage report to maven
    
    * Resolved the compareTo bug
    
    * Adding a script that will clean up the cache when a file was added into the .gitignore
    
    * Solved the bug for the scriptCleanUp file
    
    * Adding the main file to gitignore
    
    * Adding a guide for pushing
    
    * Adding test files
    
    * Adding test files
    
    * Ajout des tests
    
    * Ajout d'une ligne dans le README qui a ete supprime avant

--- src/devopsproject/Main.java
+++ src/devopsproject/Main.java
@@ -14,36 +14,18 @@ import java.util.Map;
  * @author stephanie
  */
 public class Main {
+    
+    public static void main(String [] args) throws IOException {
+        DataFrame d = new DataFrame("tests/resources/test1.csv",",");
+        d.head(5);
+/*
+    for (Map.Entry<String, Integer> entrySet : hash.entrySet()) {
+	String key = entrySet.getKey();
+	Integer value = entrySet.getValue();
 
-    public static void main(String[] args) throws IOException {
+	System.out.println(key + " "+ value);
 
-        /*DataFrame d= new DataFrame("/home/stephanie/Documents/Documents/NewFolder/DevOps/src/src/devopsproject/test.csv",",");
-        // d.show();
-        //d.head(3);
-        d.tail(2);*/
-        HashMap<String, Integer> hash = new HashMap<>();
-        hash.put("b", 1);
-        hash.put("a", 2);
-
-        for (Map.Entry<String, Integer> entrySet : hash.entrySet()) {
-            String key = entrySet.getKey();
-            Integer value = entrySet.getValue();
-
-            System.out.println(key + " " + value);
-
-        }
-        //d.head("Age", 2);
-        //d.last("Age", 2);
-        //ArrayList toprint = d.data.get("Age");
-        //System.out.println(toprint.get(0));
-        /*DataFrame d= new DataFrame("/home/stephanie/Documents/Documents/NewFolder/DevOps/src/src/devopsproject/test.csv",",");
-        // d.show();
-        //d.head(3);
-        d.tail(2);*/
-        //d.head("Age", 2);
-        //d.last("Age", 2);
-        //ArrayList toprint = d.data.get("Age");
-    }
+    }*/
      /*DataFrame d= new DataFrame("/home/stephanie/Documents/Documents/NewFolder/DevOps/src/src/devopsproject/test.csv",",");
     // d.show();
     //d.head(3);
@@ -51,5 +33,7 @@ public class Main {
     //d.head("Age", 2);
      //d.last("Age", 2);
      //ArrayList toprint = d.data.get("Age");
-     //System.out.println(d.data.size());  
+     //System.out.println(d.data.size());
+ }    
+    
 }
