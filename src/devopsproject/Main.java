3d3b1c9a8cd59c08a532138906162323bc8f4163
BapSDB <bleuzeb@etu.univ-grenoble-alpes.fr>
2018-04-14 12:44:52 +0200

    Tests de base OK pour affichage (#24)
    * ajout liste position labels
    
    * merge Constructeurs/Selecteurs Baptiste
    
    * Resolution probleme CompareTo
    
    * Resolution probleme CompareTo
    
    * Tests de Base OK pour affichage
    
    * Tests de Base OK pour affichage

--- src/devopsproject/Main.java
+++ src/devopsproject/Main.java
@@ -14,31 +14,34 @@ import java.util.Map;
  * @author stephanie
  */
 public class Main {
-    
-     public static void main(String [] args) throws IOException {
-     
-     DataFrame d= new DataFrame("Users/r_benounnas/Desktop/test.csv",",");
 
-    HashMap<String,Integer> hash = new HashMap<>();
-    hash.put("b", 1);
-    hash.put("a", 2);
+    public static void main(String[] args) throws IOException {
 
+        /*DataFrame d= new DataFrame("/home/stephanie/Documents/Documents/NewFolder/DevOps/src/src/devopsproject/test.csv",",");
+        // d.show();
+        //d.head(3);
+        d.tail(2);*/
+        HashMap<String, Integer> hash = new HashMap<>();
+        hash.put("b", 1);
+        hash.put("a", 2);
 
-    for (Map.Entry<String, Integer> entrySet : hash.entrySet()) {
-	String key = entrySet.getKey();
-	Integer value = entrySet.getValue();
+        for (Map.Entry<String, Integer> entrySet : hash.entrySet()) {
+            String key = entrySet.getKey();
+            Integer value = entrySet.getValue();
 
-	System.out.println(key + " "+ value);
+            System.out.println(key + " " + value);
 
+        }
+        //d.head("Age", 2);
+        //d.last("Age", 2);
+        //ArrayList toprint = d.data.get("Age");
+        //System.out.println(toprint.get(0));
+        /*DataFrame d= new DataFrame("/home/stephanie/Documents/Documents/NewFolder/DevOps/src/src/devopsproject/test.csv",",");
+        // d.show();
+        //d.head(3);
+        d.tail(2);*/
+        //d.head("Age", 2);
+        //d.last("Age", 2);
+        //ArrayList toprint = d.data.get("Age");
     }
-     /*DataFrame d= new DataFrame("/home/stephanie/Documents/Documents/NewFolder/DevOps/src/src/devopsproject/test.csv",",");
-    // d.show();
-    //d.head(3);
-    d.tail(2);*/
-    //d.head("Age", 2);
-     //d.last("Age", 2);
-     //ArrayList toprint = d.data.get("Age");
-     System.out.println(d.data.size());
- }    
-    
 }
--- tests/testsPackage/testsPackage/TestsDataframe.java
+++ tests/testsPackage/testsPackage/TestsDataframe.java
@@ -1,49 +1,48 @@
 package testsPackage;
 
 import devopsproject.DataFrame;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.After;
-import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.junit.Rule;
 
+@SuppressWarnings("rawtypes")
 public class TestsDataframe {
-    
+
     DataFrame df;
-    
+
     public TestsDataframe() {
-        
+
     }
-    
+
     @Before
     public void setUp() {
-        
+
     }
-    
+
     @After
     public void tearDown() {
-        
+
     }
-    
+
     @Test
-    public void dataframeCreationEmptyConstructor(){
+    public void dataframeCreationEmptyConstructor() {
         df = new DataFrame();
     }
-    
+
     @Test
-    public void dataframeCreationLists(){
-        String [] labels = new String[3];
+    public void dataframeCreationLists() {
+        String[] labels = new String[3];
         labels[0] = String.valueOf('a');
         labels[1] = String.valueOf('b');
         labels[2] = String.valueOf('c');
-        
+
         List<String> element1 = new ArrayList<>();
-        element1.add("s1");element1.add("s2");element1.add("s3");
+        element1.add("s1");
+        element1.add("s2");
+        element1.add("s3");
 
         List<Integer> element2 = new ArrayList<>();
         element2.add(1);
@@ -54,17 +53,27 @@ public class TestsDataframe {
         element3.add((float) 1.4);
         element3.add((float) 2.4);
         element3.add((float) 3.5);
-        
-        List <List> elements = new ArrayList<>();
+
+        List<List> elements = new ArrayList<>();
         elements.add(element1);
         elements.add(element2);
         elements.add(element3);
-        
+
         df = new DataFrame(labels, elements);
+        df.head(2);
+        System.out.println("");
+        df.show();
+        System.out.println("");
+        df.tail(2);
     }
-    
+
     @Test
-    public void dataframeCreationFile() throws IOException{
-        df = new DataFrame("resources/file1.csv", ",");
+    public void dataframeCreationFile() throws IOException {
+        df = new DataFrame("tests/testsPackage/resources/file1.csv", ",");
+        df.head(2);
+        System.out.println("");
+        df.show();
+        System.out.println("");
+        df.tail(2);
     }
 }
